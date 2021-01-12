42b6b87f81d66ccdb4f3cb74b75945c87cbefd4a
jackchou <jackzhou4job@163.com>
2021-01-12 21:18:31 +0800

    注册全局组件

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,17 @@
  * @Description: 注册全局组件
  * @Date: 2021-01-09 21:06:09 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-01-10 02:05:29 +0800
+ * @LastEditTime: 2021-01-12 21:15:45 +0800
  * @LastEditors: JackChou
  */
 import WatchTest from './WatchTest.vue'
 import WatchEffect from './WatchEffect.vue'
 import SetupOne from './SetupOne.vue'
 import SetupTwo from './SetupTwo.vue'
-export default [WatchTest, WatchEffect, SetupOne, SetupTwo]
+const components = [WatchTest, WatchEffect, SetupOne, SetupTwo]
+export const registerComponents = (app) => {
+  components.forEach((component) => {
+    app.component(component.name, component)
+  })
+}
+export default components
--- src/main.js
+++ src/main.js
@@ -2,18 +2,15 @@
  * @Description: 入口文件
  * @Date: 2020-12-22 21:05:26 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-01-12 20:47:56 +0800
+ * @LastEditTime: 2021-01-12 21:17:52 +0800
  * @LastEditors: JackChou
  */
 import { createApp } from 'vue'
 import App from './App.vue'
-import components from './components'
+import { registerComponents } from './components'
 // 接受一个组件或者组件定义对象作为参数，返回一个组件的实例
 const vue = createApp(App)
-// 注册全局组件
-components.forEach((component) => {
-  vue.component(component.name, component)
-})
+registerComponents(vue)
 // 使用这个组件库
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
