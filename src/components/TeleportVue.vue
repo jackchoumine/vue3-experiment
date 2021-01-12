bb52b43efe7a07addfb8c6ff8af8a148caf04b0a
jackchou <jackzhou4job@163.com>
2021-01-12 21:58:41 +0800

    teleport用法

---
+++ src/components/TeleportVue.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <a-button @click="isOpen = true">打开</a-button>
+    <Teleport to=".modal">
+      <div v-if="isOpen" class="modal">
+        <h2>Teleport组件</h2>
+        <div>body</div>
+        <p>modal footer</p>
+        <a-button @click="isOpen = false">关闭</a-button>
+      </div>
+    </Teleport>
+  </div>
+</template>
+<script>
+import { defineComponent, ref, reactive, watch, computed } from 'vue'
+export default defineComponent({
+  name: 'TeleportVue',
+  components: {},
+  setup(props, { emit, attrs, slots }) {
+    const isOpen = ref(false)
+    return { isOpen }
+  },
+})
+</script>
+<style scoped lang="css">
+.modal {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  z-index: 999;
+  background-color: gray;
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,14 +2,15 @@
  * @Description: 注册全局组件
  * @Date: 2021-01-09 21:06:09 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-01-12 21:15:45 +0800
+ * @LastEditTime: 2021-01-12 21:43:33 +0800
  * @LastEditors: JackChou
  */
 import WatchTest from './WatchTest.vue'
 import WatchEffect from './WatchEffect.vue'
 import SetupOne from './SetupOne.vue'
 import SetupTwo from './SetupTwo.vue'
-const components = [WatchTest, WatchEffect, SetupOne, SetupTwo]
+import TeleportVue from './TeleportVue.vue'
+const components = [WatchTest, WatchEffect, TeleportVue, SetupOne, SetupTwo]
 export const registerComponents = (app) => {
   components.forEach((component) => {
     app.component(component.name, component)
