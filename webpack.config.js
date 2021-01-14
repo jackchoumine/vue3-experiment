0e2c83903f86a3444c88180124b4377e3367ac56
jack <jackzhoumine@gmail.com>
2021-01-14 11:22:15 +0800

    格式化webpack设置

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,62 @@ const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = (env = {}) => ({
-  mode: env.prod ? 'production' : 'development',
-  devtool: env.prod ? 'source-map' : 'eval-cheap-module-source-map',
-  entry: path.resolve(__dirname, './src/main.js'),
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        use: 'vue-loader'
+module.exports = (env = {}) => {
+  console.log(env)
+  return {
+    mode: env.prod ? 'production' : 'development',
+    devtool: env.prod ? 'source-map' : 'eval-cheap-module-source-map',
+    entry: path.resolve(__dirname, './src/main.js'),
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/dist/',
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
-      {
-        test: /\.png$/,
-        use: {
-          loader: 'url-loader',
-          options: { limit: 8192 }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: { hmr: !env.prod }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: 'vue-loader',
+        },
+        {
+          test: /\.png$/,
+          use: {
+            loader: 'url-loader',
+            options: { limit: 8192 },
           },
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
-    new webpack.DefinePlugin({
-      __VUE_OPTIONS_API__: 'true',
-      __VUE_PROD_DEVTOOLS__: 'false'
-    })
-  ],
-  devServer: {
-    inline: true,
-    hot: true,
-    stats: 'minimal',
-    contentBase: __dirname,
-    overlay: true
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: { hmr: !env.prod },
+            },
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+      }),
+    ],
+    devServer: {
+      inline: true,
+      hot: true,
+      stats: 'minimal',
+      contentBase: __dirname,
+      overlay: true,
+    },
   }
-})
+}
