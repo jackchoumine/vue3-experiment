181eb85d39508f559f2ae6d78ce6e234ea638dfa
jack <jackzhoumine@gmail.com>
2021-03-18 15:56:26 +0800

    feat: 修改main

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
+/*
+ * @Description:
+ * @Hash: 不是路由组件
+ * @Date: 2021-01-14 11:14:49 +0800
+ * @Author: JackChou
+ * @LastEditTime: 2021-03-18 15:51:55 +0800
+ * @LastEditors: JackChou
+ */
 const path = require('path')
 const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env = {}) => {
+  console.log('env')
   console.log(env)
   return {
-    mode: env.prod ? 'production' : 'development',
+    // mode: env.prod ? 'production' : 'development',
+    mode: 'development',
     devtool: env.prod ? 'source-map' : 'eval-cheap-module-source-map',
     entry: path.resolve(__dirname, './src/main.js'),
     output: {
@@ -56,6 +66,7 @@ module.exports = (env = {}) => {
     devServer: {
       inline: true,
       hot: true,
+      open: true,
       stats: 'minimal',
       contentBase: __dirname,
       overlay: true,
